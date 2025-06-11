070d4bbb13c9a12745443ed959a2ba74b4f18d86
Andrei LAPUSNEANU <alapusneanu@SIUB9245.local>
2025-06-11 14:02:40 +0300

    Added functionality to download dummy assets on the system before opening them

--- Techniques/User_Activity/open_documents.sh
+++ Techniques/User_Activity/open_documents.sh
@@ -1,9 +1,17 @@
 #!/bin/zsh
 
-# Populate the system with dummy files of various formats
-
-
-
+FILENAMES=(
+    "avatar.jpeg"
+    "Budget.xlsx"
+    "Agreement.doc"
+    "dog.jpg"
+    "family1.jpg"
+    "family2.jpg"
+    "Letter.docx"
+    "passport.png"
+    "Passwords.txt"
+    "Presentation.pptx"
+)
 
 EXTENSIONS=(
     "pdf" 
@@ -24,6 +32,29 @@ DIRECTORIES=(
     "$HOME/Downloads"
 )
 
+# Populate the system with dummy files of various formats
+SOURCE_REPO="https://github.com/Lapus0108/macOS-evasion-research/raw/refs/heads/main/Techniques/User_Activity/Assets/"
+
+TEMP_DIR="/tmp/dummy_assets"
+mkdir -p "$TEMP_DIR"
+
+for file in "${FILENAMES[@]}"; do
+    temp_file="/$TEMP_DIR/$file"
+    curl -s -L -o "$temp_file" "${SOURCE_REPO}${file}"
+
+    if [[ $? -ne 0 ]]; then
+        echo "Failed to download $file"
+        continue
+    fi
+
+    for destination_dir in "${DIRECTORIES[@]}"; do
+        cp "$temp_file" "$destination_dir/"
+    done
+done
+
+rm -rf "$TEMP_DIR"
+
+# Open the files with their default applications
 count=0
 
 for dir in "${DIRECTORIES[@]}"; do
